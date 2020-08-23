8257d09ed0d075604827a70a72e0287d38572859
SaKu2110 <b1018106@fun.ac.jp>
2020-08-23 15:57:48 +0900

    Add skeleton test code.

--- debug_test.go
+++ debug_test.go
@@ -1,1 +1,13 @@
 package golexa
+
+import(
+	"testing"
+
+	_ "github.com/stretchr/testify/assert"
+)
+
+func TestHandledebugPrint(t *testing.T) {
+}
+
+func TestDebugPrint(t *testing.T) {
+}
--- golexa_test.go
+++ golexa_test.go
@@ -1,1 +1,30 @@
 package golexa
+
+import(
+	"testing"
+
+	_ "github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+}
+
+/****************************/
+/* Intent handler registers */
+/****************************/
+
+func TestLaunchSession(t *testing.T) {
+}
+
+func TestClosedSession(t *testing.T) {
+}
+
+func TestCustomIntent(t *testing.T) {
+}
+
+/***************************/
+/* Response Json generator */
+/***************************/
+
+func TestResponse(t *testing.T) {
+}
