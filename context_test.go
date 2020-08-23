8633135f59db91863a31baf23da8d238be2ddfb1
SaKu2110 <b1018106@fun.ac.jp>
2020-08-23 15:50:25 +0900

    Add skeleton test code.

--- context_test.go
+++ context_test.go
@@ -1,1 +1,70 @@
 package golexa
+
+import(
+	"testing"
+
+	_ "github.com/stretchr/testify/assert"
+)
+
+func TestGenContext(t *testing.T) {
+}
+
+func TestParseRequest(t *testing.T) {
+}
+
+func TestMakeNewResponse(t *testing.T) {
+}
+
+func TestPackData(t *testing.T) {
+}
+
+func TestJSON(t *testing.T) {
+}
+
+/******************************/
+/* OutputSpeech object setter */
+/******************************/
+
+func TestAsk(t *testing.T) {
+}
+
+func TestText(t *testing.T) {
+}
+
+func TestWithText(t *testing.T) {
+}
+
+func TestWithSSML(t *testing.T) {
+}
+
+func TestRepromptWithText(t *testing.T) {
+}
+
+func TestRepromptWithSSML(t *testing.T) {
+}
+
+func TestSetOutputSpeech(t *testing.T) {
+}
+
+func TestCheckBehavior(t *testing.T) {
+}
+
+/**********************/
+/* Card object setter */
+/**********************/
+
+func TestSimpleCard(t *testing.T) {
+}
+
+func TestStandardCard(t *testing.T) {
+}
+
+func TestLinkAccountCard(t *testing.T) {
+}
+
+/*********************/
+/* Slot value getter */
+/*********************/
+
+func TestSlotLoader(t *testing.T) {
+}
